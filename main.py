4fb5f5cd1ab4c2cd05b3ae00b59bdf9c21d81743
JewelsCo18 <39061516+JewelsCo18@users.noreply.github.com>
2018-10-11 16:10:56 -0400

    updated main python file

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import pyttsx3 as tts
 import cv2
 import numpy 
 
-video = cv2.VideoCapture('testvideo.avi')
+video = cv2.VideoCapture(0)
 
-#template matching with video: https://stackoverflow.com/questions/42559985/python-opencv-template-matching-using-the-live-camera-feed-frame-as-input
-
+#colours
 while(1):
 
     # Take each frame
@@ -15,26 +14,64 @@ while(1):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
-    blue_min = np.array([110,50,50])
-    blue_max = np.array([130,255,255])
+    # range of blue 
+    blue_min = np.array([121,91,39])
+    blue_mid = np.array([173,134,66])
+    blue_max = np.array([232,199,141])
+
+    #range of orange
+    orange_min = np.array([51,92,205])
+    orange_mid = np.array([67,115,251])
+    orange_max = np.array([115,152,255])
+
+    #range of red
+    red_min = np.array([32,36,98])
+    red_mid = np.array([53,59,154])
+    red_max = np.array([121,125,197])
+
+    #range of purple
+    purp_min = np.array([63,29,64])
+    purp_mid = np.array([105,53,107])
+    purp_max = np.array([181,114,184])
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, blue_min, blue_max)
+    blue = cv2.inRange(hsv, blue_min, blue_max)
+    orange = cv2.inRange(hsv, orange_min, orange_max)
+    red = cv2.inRange(hsv, red_min, red_max)
+    purple = cv2.inRange(hsv, purp_min, purp_max)
 
     # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    res_blue = cv2.bitwise_and(frame,frame, mask= blue)
+    res_orange = cv2.bitwise_and(frame,frame, mask= orange)
+    res_red = cv2.bitwise_and(frame,frame, mask= red)
+    res_purple = cv2.bitwise_and(frame,frame, mask= purple)
 
 #understand what this is attempting to do 
-    """
+
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     cv2.imshow('res',res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-    """
+
+#camera capture
+"""
+while(True):
     
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+"""
+
+# When everything done, release the capture
 video.release()
 cv2.destroyAllWindows()
 
@@ -45,3 +82,4 @@ text = tts.init()
 text.say("I will speak this text")
 text.runAndWait()
 """
+
