2f3c1e9518b2cc043a4797560a9b48a2d2e07cce
JewelsCo18 <39061516+JewelsCo18@users.noreply.github.com>
2018-11-01 19:09:02 -0400

    fueled nightmares

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import serial as serialkiller
 
 video = cv2.VideoCapture(0)
 
-#Running program until broken by using escape key
-while(1):
+def resize(frame, width=680, height=440):
+    return cv2.resize(frame, (width, height))
 
-    # Take each frame
-    _, frame = video.read()
+def find_centers(frame):
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -19,38 +18,58 @@ while(1):
     maximumVal = {'red':(186,255,255), 'light-blue':(117,255,255), 'green':(86,255,255), 'yellow':(30,255,255), 'pink':(255,255,255), 'white':(0,0,255)}
 
     colors = {'red':(0,0,255), 'light-blue':(255,0,0), 'green':(0,255,0), 'yellow':(0,255,255), 'pink':(255,0,255), 'white':(255,255,255)}
+
+    colorCoord = {}
     
-    for key, value in maximumVal.items():
+    for key in maximumVal.keys():
 
         mask = cv2.inRange(hsv, minimumVal[key], maximumVal[key])
 
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
-
-        # finding contours then creating the circle
         if len(cnts) > 0:
             # find the largest contour in the mask, then use
             # it to compute the minimum enclosing circle and
             # centroid
             centroid = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(centroid)
-            M = cv2.moments(centroid)
-            #center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-            # draw the circle and centroid on the frame, updates position
-            if radius > 0.5:
-                cv2.circle(frame, (int(x), int(y)), int(radius), colors[key], 2)
-                cv2.putText(frame,key + " ball", (int(x-radius),int(y-radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,colors[key],2)
-                                
+            colorCoord[key] = [x,y]
 
-    cv2.imshow('frame',frame)
-    
+            creating_circles(radius, x, y, colors, key, frame)
+            
+    print(colorCoord)
+    return colorCoord
+
+def creating_circles(radius, x, y, colors, key, frame):
+    # find in contours then creating the circle
+        # draw the circle and centroid on the frame, updates position
+    if radius > 0.5:
+        cv2.circle(frame, (int(x), int(y)), int(radius), colors[key], 2)
+        cv2.putText(frame,key + " ball", (int(x-radius),int(y-radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,colors[key],2)
+
+#Running program until broken by using escape key
+while(1):
+
+    # Take each frame
+    _, frame = video.read(0)
+    frame = resize(frame)
+    colorCoord = find_centers(frame)
+
+    cv2.imshow('frame',frame)     
+            
+
+    #escape
     k = cv2.waitKey(1) & 0xFF
     if k == 27:#escape key
         print("Quit")
         video.release()
         cv2.destroyAllWindows()
+        question = input("Enter")
+        if question == "go":
+            check = colorCoord['light-blue'][0] - colorCoord['yellow'][0]
+            if 102 <= check <= 128:
+                print("done")
         break
 
 
@@ -60,4 +79,3 @@ text = tts.init()
 text.say("I will speak this text")
 text.runAndWait()
 """
-
